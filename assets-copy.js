1b0317e8782db3c74b3ff5547fe56bba76c6499e
Deeksha Deepak <deekshad1502@gmail.com>
2025-09-18 12:09:15 +0530

    reverted asset copy js file

--- assets-copy.js
+++ assets-copy.js
@@ -1,34 +1,18 @@
 const fs = require('fs-extra');
 (async () => {
     try {
-        var source = "projects/sunbird-video-player/node_modules/@project-sunbird/sunbird-player-sdk-v9/lib/assets";
+      var source = "node_modules/@project-sunbird/sunbird-player-sdk-v9/lib/assets";
+        const dest = "dist/sunbird-video-player/lib/assets/";
+        const isAssetsExists = await fs.pathExists(dest)
         var libsource = "projects/sunbird-video-player/src/lib/assets";
-
-        const libDest = "dist/sunbird-video-player/lib/assets/"; 
-        // Web component assets destination  
-        const wcDest = "dist/video-player-wc/assets/";
-        // Copy for library build
-        const isLibAssetsExists = await fs.pathExists(libDest)
-        if (isLibAssetsExists) {
-            await fs.remove(libDest);
+        if (isAssetsExists) {
+            await fs.remove(dest);
         }
-        await fs.ensureDir(libDest);
-        
-        // Copy for web component build
-        await fs.ensureDir(wcDest);
-        
-        // Copy to both destinations if sources exist
-        if (await fs.pathExists(source)) {
-            await fs.copy(source, libDest);
-            await fs.copy(source, wcDest);
-        }
-        if (await fs.pathExists(libsource)) {
-            await fs.copy(libsource, libDest);
-            await fs.copy(libsource, wcDest);
-        }
-        
-        console.log('Assets copied successfully to both destinations');
+        await fs.ensureDir(dest);
+        await fs.copy(source, dest)
+        await fs.copy(libsource, dest)
+        console.log('Assets copied successfully')
     } catch (err) {
-        console.error("Error while copying assets", err);
+        console.error("Error while copying assets", err)
     }
 })();
